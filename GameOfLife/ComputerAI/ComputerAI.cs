132043fd75bd041d99ce6ef6dcc18aeefc31f6e0
Elkor1991 <mol1991@hotmail.com>
2014-11-18 10:21:42 +0100

    ComputerAI projekt hozzáadva

---
+++ GameOfLife/ComputerAI/ComputerAI.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Input;
+using GameOfLife.DataModel;
+
+namespace GameOfLife.ComputerAI
+{
+    public class ComputerAI
+    {
+        private DataModel.DataModel model;
+
+        public ComputerAI(DataModel.DataModel receivedModel)
+        {
+            model = receivedModel;
+        }
+
+        public Keys atFork()
+        {
+            return 0;
+        }
+
+        public Keys blueFieldChangeJob()
+        {
+            return 0;
+        }
+
+        public Keys blueFieldSwitchSalary()
+        {
+            return 0;
+        }
+
+        public Keys buyStock()
+        {
+            return 0;
+        }
+
+        public Keys computerTurn()
+        {
+            return 0;
+        }
+
+        public Keys firstFork()
+        {
+            return 0;
+        }
+
+        public Keys selectJob(Int32[] jobs)
+        {
+            return 0;
+        }
+
+        public Keys selectRetire()
+        {
+            return 0;
+        }
+
+        public Keys selectSalary(Int32[] salaries)
+        {
+            return 0;
+        }
+
+        public Keys switchSalary()
+        {
+            return 0;
+        }
+
+
+    }
+}
