086d26ff29a726f13b7074d5b27cf064b58469a5
Elkor1991 <mol1991@hotmail.com>
2014-12-01 15:21:34 +0100

    ComputerAI-ban kisebb változtatások

--- GameOfLife/ComputerAI/ComputerAI.cs
+++ GameOfLife/ComputerAI/ComputerAI.cs
@@ -17,30 +17,7 @@ namespace GameOfLife.ComputerAI
             model = receivedModel;
         }
 
-        public Keys atFork()
-        {
-            //return 0;
-            return Keys.D1; //T.SZ. Visszatérési érték: Keys.D1 vagy Keys.D2
-        }
-
-        public /*Keys*/ bool blueFieldChangeJob()
-        {
-            //return 0;
-            return true; //T.SZ. elég egy sima bool érték, hogy vált-e munkát vagy sem
-        }
-
-        public bool blueFieldTradeSalary()//Keys blueFieldSwitchSalary()
-        {
-            //return 0;
-            return true; //T.SZ. elég egy bool érték, és mindenhol Trade-ként hívjuk, legyen itt is Trade
-        }
-
-        public Int32 /*Keys*/ buyStock()
-        {
-            return 0; //T.SZ. visszatérési érték: 0-8 között egy szám, hogy melyik részvényt akarja megvenni. Olyat vegyen, ami még nem foglalt! Csak akkor vegyen, ha van pénze!
-        }
-
-        public Int32/*Keys*/ computerTurn()
+        public Int32 computerTurn()
         {
             /* T.SZ. Visszatérési érték a menüpont sorszáma (0-4 között):
              * 0 - Pörgetés
@@ -56,32 +33,66 @@ namespace GameOfLife.ComputerAI
 
         public Keys firstFork()
         {
-            //return 0;
-            return Keys.D1; //T.SZ. visszatérési érték: Keys.D1 vagy Keys.D2
+            Random rnd = new Random();
+            Int32 decision = rnd.Next(1, 3);
+            if (decision == 1)
+            {
+                return Keys.D1;
+            }
+            else
+            {
+                return Keys.D2;
+            }
         }
 
-        public /*Keys*/ Int32 selectJob(List<Int32> jobs) //T.SZ. Változott a modell, List<Int32>-t fogok adni int[] helyett
+        public Keys atFork()
+        {
+            Random rnd = new Random();
+            Int32 decision = rnd.Next(1, 3);
+            if (decision == 1)
+            {
+                return Keys.D1;
+            }
+            else
+            {
+                return Keys.D2;
+            }
+        }
+
+        public bool blueFieldChangeJob()
+        {
+            return true; //T.SZ. elég egy sima bool érték, hogy vált-e munkát vagy sem
+        }
+
+        public bool blueFieldTradeSalary()
+        {
+            return true; //T.SZ. elég egy bool érték, és mindenhol Trade-ként hívjuk, legyen itt is Trade
+        }
+
+        public Int32 buyStock()
+        {
+            return 0; //T.SZ. visszatérési érték: 0-8 között egy szám, hogy melyik részvényt akarja megvenni. Olyat vegyen, ami még nem foglalt! Csak akkor vegyen, ha van pénze!
+        }
+
+        public Int32 selectJob(List<Int32> jobs)
         {
             return 0; //T.SZ. visszatérési érték: 0,1,2. Attól függően, hogy hanyadik munkát akarja
         }
 
-        public Keys selectRetire()
-        {
-            //return 0;
-            return Keys.D1; //T.SZ. visszatérési érték: Keys.D1, ha vidéki ház; Keys.D2, ha milliomosok háza
-        }
-
-        public /*Keys*/ Int32 selectSalary(List<Int32> salaries) //T.SZ. Változott a modell, List<Int32>-t fogok adni int[] helyett
+        public Int32 selectSalary(List<Int32> salaries)
         {
             return 0; //T.SZ. visszatérési érték: 0,1,2. Attól függően, hogy hanyadik fizetést akarja
         }
 
-        public Int32 tradeSalary()//Keys switchSalary()
+        public Int32 tradeSalary()
         {
             return 0; //T.SZ. itt is legyen trade, mert mindenhol Trade-nek hívjuk. Visszatérési érték: a játékos sorszáma, akivel cserélni akar (0-5 között, lásd update függvény, TRADEWITHWHO region)
             //T.SZ. olyan játékost válasszon, aki nem önmaga, és létező játékos, pl. ha ketten játszanak, ne válassza a 3. játékost
         }
 
-
+        public Keys selectRetire()
+        {
+            return Keys.D1; //T.SZ. visszatérési érték: Keys.D1, ha vidéki ház; Keys.D2, ha milliomosok háza
+        }
     }
 }
